2cc83a9c37493b995b343c392f741ad89c4e028d
PoYun1053892 <albert199811@gmail.com>
2021-03-18 20:18:31 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,228 +8,245 @@ const {
 
 
 
-//指令gulp do 會去跑do的東西
+
+//======  1 . console.log
 function defaultTask(cb) {
-    //任務會寫在這
+    //任務
     console.log('hello gulp4');
     cb();
 }
-// 輸出任務
+//輸出任務
 exports.do = defaultTask;
 
-// 2.任務流程
 
-//任務A
-function funcA(cb){
-    console.log("hihiop");
+// ====== 2. 任務流程
+
+// 任務a
+function funcA(cb) {
+    //任務a
+    console.log('任務a');
     cb();
 }
-function funcB(cb){
-    console.log("hihigaygaygaygay");
+
+// 任務b
+function funcB(cb) {
+    //任務b
+    console.log('任務b');
     cb();
 }
 
+exports.doA = series(funcA, funcB); // 任務串連
+exports.doB = parallel(funcA, funcB); // 任務並行
 
-exports.doA = series(funcA,funcB);//任務串聯執行
-exports.doB = parallel(funcA,funcB);//任務並聯執行
 
+// ========== 3. 打包 src / dest
 
-// 3.打包 src/dest
-
-function movefile(){
-    return src('js/main.js').pipe(dest('output'));//src檔案來源/目的地->輸出地
+// js
+function movefile() {
+    return src('js/main.js').pipe(dest('output'));  // src -> dest
 }
-function movecss(){
-    return src('css/sty.css').pipe(dest('output'));//src檔案來源/目的地->輸出地
-    
-}
-exports.cp = movefile;
-
-// 4.watch 監看
-
-function watchtask(){
-   watch('css/sty.css',movecss);//也可以監看多支檔案有沒有變動
-    watch('js/main.js',movefile);//當main.js有變動時-->執行movefile任務
-}
-exports.gaygay = watchtask;
-
-function watchall(){//同時監控與打包成mini
-    watch (['css/sty.css','js/main.js']
-    ,parallel(mini_css,uqlity_js));
+// css
+function movefile_css() {
+    return src('css/style.css').pipe(dest('output/css'));  // src -> dest
 }
 
+// exports.cp = movefile; // 輸出任務
+
+
+// ========== 4. watch  監看 
+
+function watchTask() {
+    watch('js/main.js', movefile);  // 當main.js 有變動 -> 執行movefile任務 
+    watch('css/style.css', movefile_css);  // css 變動 
+}
+
+exports.dowatch = watchTask; //輸出任務
+
+
+
+
+//====== 5.壓縮 js
+//  套件引入
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+
+
+
+function uglify_js() {
+    return src('js/main.js')
+        .pipe(uglify()) // 去執行uglify函式
+        .pipe(rename({
+            //extname : '.min.js' //修改副檔名
+            basename: 'scripts' // 改檔名
+        }))
+        .pipe(dest('output/mini'));
+}
+
+// exports.js = uglify_js;
+
+// ==== 6. css 壓縮
+
+const cleanCSS = require('gulp-clean-css'); // 1. 
+
+function mini_css() {
+    return src('css/style.css')
+        .pipe(cleanCSS({ compatibility: 'ie10' })) // 
+        .pipe(rename({
+            extname: '.min.css' //修改副檔名
+            //basename : 'scripts' // 改檔名
+        }))
+        .pipe(dest('output/css'))
+}
+
+// exports.css = mini_css; // 
+
+function watchall() {
+    watch(['css/style.css', 'js/main.js'], parallel(mini_css, uglify_js)) // css js 同時監看
+}
+// 
 exports.doall = watchall;
 
-// 5.壓縮js檔案  uqlity
-const uglify = require('gulp-uglify');//這東西要這樣給他變數去接用他，引入
-const rename = require('gulp-rename');//這會去修改副檔名
-
-function uqlity_js(){
-    return src('js/main.js')
-    .pipe(uglify())
-    // .pipe(rename({
-        // extname:'.min.js'修改副檔名
-        // basename:'scipet'修改檔名
-    // }))
-    .pipe(dest('output/mini'));//src檔案來源/目的地->輸出地壓縮檔
-
-}
-exports.jsmini = uqlity_js;//exports.自行命名執行名子 = 要執行的程式名子
 
 
-// 6.壓縮css
-
-const cleanCSS = require('gulp-clean-css');
-
-function mini_css(){
-    return src('css/sty.css')
-    .pipe(cleanCSS({compatibility:'ie10'}))
-    .pipe(dest('output/css/mini'));
-}
-
-exports.cssmini = mini_css;
-
-
-// 7.合併所有的css/js檔案程式碼
-
+//合併程式碼
 const concat = require('gulp-concat');
 
-function cssconcat(){
-    return src('css/*.css')//也可以指定不合併某檔案(['*.css','!css/某.css'])
-    .pipe(concat('all.css'))
-    // .pipe(cleanCSS({compatibility:'ie10'})) 也可以合併完接者壓縮檔案
-    .pipe(dest('output/css'));
+function cssconcat() {
+    return src(['css/*.css', '!css/style.css'])
+        .pipe(concat('all.css')) //合併
+        .pipe(cleanCSS({ compatibility: 'ie10' })) // 壓縮
+        .pipe(dest('output/css'))
 }
 
 exports.concat = cssconcat;
 
-//sass編譯
-// npm install node-sass gulp-sass --save-dev
-const sass = require('gulp-sass');
+
+// sass 編譯 與 sourcemap 使用
+
+const sass = require("gulp-sass");
 const sourcemaps = require('gulp-sourcemaps');
 
-function styleSass(){
-    return src('sass/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle:'expanded'//nested 巢狀|expanded | compr
-    }).on('error',sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(dest('output/css'));
-    
+function styleSass() {
+    return src('dev/sass/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded' // nested 巢狀 | expanded  | compressed 壓縮
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(dest('dist/css'))
 }
 
 exports.style = styleSass;
 
-// html template 可將兩支HTML合併
-// npm install --save-dev gulp-file-include
+// html template
+
 const fileinclude = require('gulp-file-include');
 
-function htmlTemplate(){
+function htmlTemplate() {
     return src('dev/*.html')
-    .pipe(fileinclude({
-        prefix: '@@',
-        basepath: '@file'
-    }))
-    .pipe(dest('./dist'));
-
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(dest('./'))
 }
-exports.htmlall = htmlTemplate;
+
+exports.html = htmlTemplate;
 
 
-// 套件清除舊檔案
-// npm install --save-dev gulp-clean
+//套件清除舊檔案
 
 const clean = require('gulp-clean');
-function clearfile(){
-    return src('css',{read:false,allowEmpty:true})//allowEmpty:true 允許資料夾不存在也能刪除
-    .pipe(clean({force:true}));//{force:true}強制刪除
+
+
+function clearfile() {
+    return src('css', { read: false, allowEmpty: true })  // allowEmpty : true 允許空的資料結構
+        .pipe(clean({ force: true }));  // {force: true} 強制刪除
 }
+
 // exports.clear = clearfile;
-exports.pcss = series(clearfile,styleSass);//先刪除css資料夾，再打包sass
+
+exports.pcss = series(clearfile, styleSass)  //先刪除舊檔案，再進行打包 
 
 
-//壓縮圖片 不過這很吃效能，圖片1mb以下使用較好
-// npm install --save-dev gulp-imagemin
+// 壓縮圖片 https://www.npmjs.com/package/gulp-imagemin
 
 const imagemin = require('gulp-imagemin');
 
-function imgs(){
+function imgs() {
     return src('images/*.*')
-    .pipe(imagemin())
-    // .pipe(rename({
-        //prefix:"bonjour-",檔名前
-        //suffix:"-min",檔名後
-        // extname:'.min.css'修改副檔名
-        //basename:'scripts'改檔名
-    // }))
-    .pipe(dest('images/mini/'))
+        .pipe(rename({
+            prefix: "bonjour-", // 檔名前
+            // suffix: "-min" // 檔名後
+            // extname : '.min.css' //修改副檔名
+            //basename : 'scripts' // 改檔名
+        }))
+        .pipe(imagemin())
+        .pipe(dest('images/mini/'))
 }
 
-exports.img = imgs;
+exports.minimg = imgs
 
-// function clearfile_img(){
-//     return src('images/mini',{read:false,allowEmpty:true})
-//     .pipe(clean({force:true}));
-// }
-
-// exports.clearImg = series(clearfile_img,imgs);// 先執行刪除舊圖片 ->壓縮圖片
-
-// 瀏覽器同步
-// npm install browser-sync --save-dev
-
-const browsersync = require('browser-sync').create();
-const reload = browserSync.reload;
-
-function browserSync(){
-    browsersync.init({
-        server:{
-            baseDir:"./dev/",
-            index:"index.html"
-        },
-        port:3000
-    });
-    
-    // 監看檔案-->reload broswer
-    watch('./dev/sass/*.scss',styleSass).on('change',reload);
-    watch('./dev/*.html',htmlTemplate).on('change',reload);
+function clearfile_img() {
+    return src('images/mini', { read: false, allowEmpty: true })  // allowEmpty : true 允許空的資料結構
+        .pipe(clean({ force: true }));  // {force: true} 強制刪除
 }
 
-exports.browser = browserSync;
+exports.clearImg = series(clearfile_img, imgs) // 先執行刪除舊圖片  -> 壓縮圖片
 
+// ======
 
-// 跨瀏覽器
-// npm install --save-dev gulp-autoprefixer
+// css prefixer 
 
 const autoprefixer = require('gulp-autoprefixer');
 
-function autoprefix(){
+
+function autoprefix() {
     return src('css/*.css')
-    .pipe(autoprefixer({
-        cascade:false
-    }))
-    .pipe(rename({
-        //prefix:"bonjour-",檔名前
-        suffix:"-prefix"//檔名後
-        // extname:'.min.css'修改副檔名
-        //basename:'scripts'改檔名
-    }))
-    .pipe(dest('css'))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(rename({
+            //prefix: "bonjour-", // 檔名前
+             suffix: "-prefix" // 檔名後
+            // extname : '.min.css' //修改副檔名
+            //basename : 'scripts' // 改檔名
+        }))
+        .pipe(dest('css'))
+} 
 
+exports.prefix = autoprefix
+
+const browsersync = require('browser-sync')
+const reload = browsersync.reload;
+
+
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: "./",  //根目錄
+            index: "index.html"  // 打開頁面
+        },
+        port: 3000
+    });
+    // 監看檔案 -> reload broswer
+    watch(['dev/sass/*.scss', 'dev/sass/**/*.scss'], series(clearfile, styleSass , autoprefix)).on('change', reload)
+    watch(['dev/*.html', 'dev/**/*.html'], htmlTemplate).on('change', reload)
 }
-exports.prefix = autoprefix;
 
-//babel 將JS寫法自動轉成嚴謹寫法ES6->ES5
 
-const babel= require('gulp-babel');
+exports.prefix = autoprefix 
+exports.browser = browserSync
 
-function babel5(){
-    return src('dev/js/*.js')
-    .pipe(babel({
-        presets:['@babel/env']
-    }))
-    .pipe(uglify())
-    .pipe(dest('js'));
+// babel
+const babel = require("gulp-babel");
+function babel5() {
+    return src("dev/js/*.js")
+            .pipe(babel({
+                presets: ["@babel/env"]
+            }))
+            .pipe(uglify())
+            .pipe(dest("js"));
 }
 
-exports.jsbable = babel5;
+exports.jsbabel = babel5 ;
