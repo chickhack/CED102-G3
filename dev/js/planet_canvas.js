1f2780970d1bfb1c4cce8eeb37700c81b280ef9b
luisa <iken608@gmail.com>
2021-03-22 00:28:24 +0800

    90% completed

--- dev/js/planet_canvas.js
+++ dev/js/planet_canvas.js
@@ -1,15 +1,7 @@
-function doFirst(){
-    //先跟HTML畫面產生關連，再建事件聆聽功能
-    let canvas = document.getElementById('canvas'),
-        context = canvas.getContext('2d'),
-        image = new Image();
-    image.onload = function(){
-        var w = 500,
-            h = 500;
-            image.width = w;
-            image.height = h;
-            context.drawImage(this, 0, 0 , w, h);
-    }
-    image.src = '../../dev/img/planet/mars.png'
-}
-window.addEventListener('load',doFirst);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
