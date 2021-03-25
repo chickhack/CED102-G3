74636f76dbd2698ad4154697fed0fe5166de26b1
PoYun1053892 <albert199811@gmail.com>
2021-03-26 01:42:32 +0800

    shop_order完成

--- dev/js/retract.js
+++ dev/js/retract.js
@@ -1,5 +1,20 @@
 const orderer_click = document.querySelector(".orderer_click");
 const orderer_form = document.querySelector(".orderer_form");
+
+const recipient_click = document.querySelector(".recipient_click");
+const recipient = document.querySelector(".recipient");
+
+const payment_click = document.querySelector(".payment_click");
+const payment = document.querySelector(".payment");
+
 orderer_click.addEventListener("click",function(){
     orderer_form.classList.toggle("retract");
+})
+
+recipient_click.addEventListener("click",function(){
+    recipient.classList.toggle("retract");
+})
+
+payment_click.addEventListener("click",function(){
+    payment.classList.toggle("retract");
 })
