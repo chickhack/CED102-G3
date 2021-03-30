7cd49786cc983356ddc45734f1bce9eb08031c0d
PoYun1053892 <albert199811@gmail.com>
2021-03-30 15:06:54 +0800

    change

---
+++ dev/js/crdeitCheck.js
@@ -0,0 +1,31 @@
+const creditCard = document.querySelectorAll(".credit");
+const submitBtn = document.querySelector("input[type='submit']");
+let checkCard = () => {
+    creditCard.forEach(c => {
+        let number = c.value.split("");
+        let numArr = [];
+        let total = 0;
+        number.forEach(num => {
+            if(parseInt(num) || parseInt(num) === 0){
+                numArr.push(num);
+            }
+        })
+        //判斷卡號為15或16碼
+        if(numArr.length == 16){
+            numArr.forEach((num,i) => {
+                if(i % 2 === 0){
+                    if(num * 2 > 10){
+                        total += (num * 2) - 9
+                    }else{
+                        total += parseInt(num);
+                    }
+                }else{
+                    total += parseInt(num);
+                }
+            })
+            console.log(total);
+        }
+    })
+}
+
+submitBtn.addEventListener("click", checkCard);
