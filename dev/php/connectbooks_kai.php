97d9122519b2984cbf07b52a90e1a8ed6d18cd01
kkaic <kkchuchang@gmail.com>
2021-04-09 15:55:06 +0800

    php

---
+++ dev/php/connectbooks_kai.php
@@ -0,0 +1,10 @@
+<?php
+    $dbname = "g3";
+    $user ="root";
+    $password= "root1009";
+    $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
+    $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, 
+	             PDO::ATTR_CASE=>PDO::CASE_NATURAL);
+    $pdo = new PDO($dsn, $user, $password, $options);
+    
+    ?>
