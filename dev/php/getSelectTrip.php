c7e1e82b97fb5dab3ddd907c99775a365033b703
chickhack <l357410@gmail.com>
2021-04-15 15:46:13 +0800

    後台新增資料

--- dev/php/getSelectTrip.php
+++ dev/php/getSelectTrip.php
@@ -1,20 +1,11 @@
 <?php 
 try {
 	require_once("./connectbooks_kai.php");
-<<<<<<< HEAD
-	$sql = "select * from spot";
-	$products = $pdo->query($sql);
-	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
-	echo json_encode($prodRows);
-} catch (PDOException $e) {
-	
-=======
 	$sql = "select spot_name,spot_lv,spot_price,miles,spot_pic1 from spot";
 	$tops = $pdo->query($sql);
 	$top = $tops->fetchAll(PDO::FETCH_ASSOC);
 	echo json_encode($top);
 } catch (PDOException $e) {
 	echo $e->getLine(), $e->getMessage();
->>>>>>> dev
 }
 ?>
