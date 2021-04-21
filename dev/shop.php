4ac592c9c7147349477b61321ddc551065e9e32b
PoYun1053892 <albert199811@gmail.com>
2021-04-21 22:37:46 +0800

    會員登入完成

--- dev/shop.php
+++ dev/shop.php
@@ -85,12 +85,12 @@
         </a>
     </li> 
     <li>
-      <a href="./login.php"
-        ><img
-          src="./img/icon/header/round-account-button-with-user-inside_(1).png"
-          alt=""
-          class="icon"
-      /></a>
+        <?php
+            if(isset($_SESSION['mem_no'])){?>
+                <a href="./account.php"><img src="./img/icon/header/round-account-button-with-user-inside_(1).png" alt="" class="icon"/></a>
+        <?php }else{ ?>
+                <a href="./login.php"><img src="./img/icon/header/round-account-button-with-user-inside_(1).png" alt="" class="icon"/></a>
+        <?php } ?>
     </li>
   </ul>
   <div class="burger">
