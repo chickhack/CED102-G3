767ea35d0c591080eb37bcfbd64dfd5582ea5149
MinRu <p7416540@gmial.com>
2021-04-21 10:59:42 +0800

    Merge branch 'Min' into dev

---
+++ dev/php/account/getSelectTrip.php
@@ -0,0 +1,11 @@
+<?php 
+try {
+	require_once("./connectbooks_kai.php");
+	$sql = "select *from spot where spot_s_status=1";
+	$tops = $pdo->query($sql);
+	$planets = $tops->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($planets);
+} catch (PDOException $e) {
+	echo $e->getLine(), $e->getMessage();
+}
+?>
