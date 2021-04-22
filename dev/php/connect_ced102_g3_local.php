d4dc1db9f7cb4d04d08e18739601a2949bf15f59
chickhack <l357410@gmail.com>
2021-04-22 15:27:07 +0800

    no message

--- dev/php/connect_ced102_g3_local.php
+++ dev/php/connect_ced102_g3_local.php
@@ -1,10 +1,7 @@
 <?php
     $dbname = "g3";
     $user ="root";
-    $password= "root";
-    $dbname = "G3";
-    $user ="root";
-    $password= "root";
+    $password= "rootpassword";
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_CASE=>PDO::CASE_NATURAL);
