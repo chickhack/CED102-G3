5840afb8ede82b6ce8c3590cfdf659562106ea1b
Min <>
2021-04-23 18:23:30 +0800

    日常解衝突

--- dev/php/connect_ced102_g3_local.php
+++ dev/php/connect_ced102_g3_local.php
@@ -1,22 +1,7 @@
 <?php
-<<<<<<< HEAD
     $dbname = "g3";
     $user ="root";
     $password= "root";
-=======
-<<<<<<< HEAD
-$dbname = "spaced";
-$user ="root";
-$password= "root1009";
-=======
- $dbname = "g3";
- $user ="root";
- $password= "root";
-    $dbname = "spaced";
-    $user ="root";
-    $password= "a00000000";
->>>>>>> b67c5ba8b800e59be67a4085f062e9bfd503756d
->>>>>>> 7ecc499740156129142ed4c15206f91eebb5fb61
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_CASE=>PDO::CASE_NATURAL);
