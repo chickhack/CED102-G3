9393cd1432d3646c9ffed8835badc03ef5274e17
PoYun1053892 <albert199811@gmail.com>
2021-04-23 17:37:59 +0800

    change

--- dev/php/connect_ced102_g3_local.php
+++ dev/php/connect_ced102_g3_local.php
@@ -1,7 +1,7 @@
 <?php
  $dbname = "spaced";
  $user ="root";
- $password= "root1009";
+ $password= "a00000000";
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, 
 	             PDO::ATTR_CASE=>PDO::CASE_NATURAL);
