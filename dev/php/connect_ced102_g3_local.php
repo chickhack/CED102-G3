d0a169cac4dea5295b6a0ba4cee89fa763e0edec
denis0629 <chioumay0408@gmail.com>
2021-04-26 13:46:11 +0800

    發文調整

--- dev/php/connect_ced102_g3_local.php
+++ dev/php/connect_ced102_g3_local.php
@@ -1,20 +1,7 @@
 <?php
-<<<<<<< HEAD
-<<<<<<< HEAD
-    $dbname = "spaced";
-    $user ="root";
-    $password= "a00000000";
-=======
-
-    $dbname = "g3";
-    $user ="root";
-    $password= "rootpassword";
->>>>>>> chick
-=======
     $dbname = "g3";
     $user ="root";
     $password= "dan700629";
->>>>>>> denis
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_CASE=>PDO::CASE_NATURAL);
