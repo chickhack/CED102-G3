1b7c233e42cfea144e231fd08144c3f540233a82
denis0629 <chioumay0408@gmail.com>
2021-04-26 14:19:07 +0800

    Merge branch 'dev' into denis
    # Conflicts:
    #	dev/php/connect_ced102_g3_local.php

---
+++ dev/php/account/getspot.php
@@ -0,0 +1,24 @@
+<!-- <?php 
+try {
+	require_once("../connect_ced102_g3_local.php");
+	$sql = "select *from spot";
+	$tops = $pdo->query($sql);
+	$spot1 = $tops->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($spot1);
+} catch (PDOException $e) {
+	echo $e->getLine(), $e->getMessage();
+}
+?> -->
+
+<?php
+try {
+	require_once("../connect_ced102_g3_local.php");
+	$sql = "select *from spot";
+	$products = $pdo->query($sql);
+	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
+
+	echo json_encode($prodRows);
+} catch (PDOException $e) {
+	
+}
+?>
