602d2515f0e32903e01895c036bbe613d3353299
denis0629 <chioumay0408@gmail.com>
2021-04-26 11:58:24 +0800

    head增加東西

--- dev/php/connect_ced102_g3_local.php
+++ dev/php/connect_ced102_g3_local.php
@@ -1,45 +1,7 @@
 <?php
-<<<<<<< HEAD
     $dbname = "g3";
     $user ="root";
     $password= "dan700629";
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    $dbname = "g3";
-    $user ="root";
-    $password= "rootpassword";
-<<<<<<< HEAD
-=======
-    $password= "root";
->>>>>>> Min
->>>>>>> 140c34ead856e4e788e07c2333e1a7bda72e6325
->>>>>>> 24a10dda290cd33b3978fc664573f65e22a1961c
->>>>>>> 288ee3e85b49761bc058fbe1c38b8151d385fea1
-=======
-    $password= "rootpassword";
->>>>>>> chick
-=======
- $dbname = "g3";
- $user ="root";
- $password= "root";
->>>>>>> Yi
-=======
->>>>>>> chick
-=======
-    $dbname = "spaced";
-    $user ="root";
-    $password= "root1009";
-
->>>>>>> KAI
-=======
-
-    $dbname = "g3";
-    $user ="root";
-    $password= "rootpassword";
->>>>>>> chick
->>>>>>> 1e526ad149974527e05ecd982e3ef6740116d172
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_CASE=>PDO::CASE_NATURAL);
