9419d08f5082ceed4ed906c2962b0b6dfc58931a
denis0629 <chioumay0408@gmail.com>
2021-04-26 17:14:33 +0800

    no message

--- dev/php/connect_ced102_g3_local.php
+++ dev/php/connect_ced102_g3_local.php
@@ -1,13 +1,8 @@
 <?php
-<<<<<<< HEAD
- $dbname="tibamefe_ced102g3";
- $user="tibamefe_since2021";
-$password="vwRBSb.j&K#E";
-=======
-    $dbname = "spaced";
-    $user ="root";
-    $password= "a00000000";
->>>>>>> 2fa3903cf89d92483954484aae408c66cf035a48
+ $dbname="g3";
+ $user="root";
+$password="dan700629";
+
     $dsn = "mysql:host=localhost;port=3306;dbname=$dbname;charset=utf8";
     $options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_CASE=>PDO::CASE_NATURAL);
